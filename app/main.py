65ae013d2d3beb33b0c70b8dbc025a9feae3b4aa
Patrik Åkerberg <patrik.akerbergz@gmail.com>
2026-01-13 10:42:36 +0100

    Persist message with SQLite via SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from app.routers.messages import router as messages_router
+from app.database import engine
+from app.models import Message
+from app.database import Base
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"status": "online"}
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(messages_router)
--- app/routers/messages.py
+++ app/routers/messages.py
@@ -1,20 +1,25 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.schemas import MessageIn, MessageOut
-from app import db
+from app.models import Message
+from app.deps import get_db
 
 router = APIRouter(prefix="/messages", tags=["messages"])
 
 @router.get("", response_model=List[MessageOut])
-def list_messages():
-    return db.MESSAGES
+def read_messages(db: Session = Depends(get_db)):
+    return db.query(Message).all()
+
 
 @router.post("", response_model=MessageOut, status_code=201)
-def create_message(message: MessageIn):
-    new_item = {"id": db.NEXT_ID, "text": message.text}
-    db.MESSAGES.append(new_item)
-    db.NEXT_ID += 1
-    return new_item
+def create_message(message: MessageIn, db: Session = Depends(get_db)
+):
+    item = Message(text=message.text)
+    db.add(item)
+    db.commit()
+    db.refresh(item)
+    return item
 
