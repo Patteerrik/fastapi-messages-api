2fed9dc9c5e60b145e98ae3936c4a86b023316ca
Patrik Åkerberg <patrik.akerbergz@gmail.com>
2026-01-22 11:11:03 +0100

    Create database tables on startup

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+
 from app.routers.messages import router as messages_router
 from app.database import engine
-from app.models import Message
+from app.models import Message, Base
 from app.database import Base
 from fastapi import Depends
 from app.core.security import require_api_key
+from app import models
 
 
 app = FastAPI()
+
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
